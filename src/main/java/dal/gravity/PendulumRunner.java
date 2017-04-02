9fdae6e54af59b491c50ee803d0afd6b40ff34a3
Armani Nosoudi <nosoudi@dal.ca>
2017-04-02 16:28:21 -0300

    RefactoringStep3 to add Jupiter by Armani!

--- src/main/java/dal/gravity/PendulumRunner.java
+++ src/main/java/dal/gravity/PendulumRunner.java
@@ -9,6 +9,7 @@ import java.text.NumberFormat;
 public class PendulumRunner {
 
 	public static final double GRAVITY = 9.80665;
+	public static final double GRAVITYj = 25;
 	
     public static void main (String [] args) {
 	NumberFormat nf = NumberFormat.getInstance ();
@@ -16,6 +17,7 @@ public class PendulumRunner {
 
 	
 	GravityModel earthG = new GravityConstant(GRAVITY);
+	GravityModel jupiterG = new GravityConstant(GRAVITYj);
 	double delta = (args.length == 0) ? .1 : Double.parseDouble (args[0]);
 	double sLen = 10, pMass = 10, theta0 = Math.PI/30;
 	RegularPendulum rp = new RegularPendulum (sLen, pMass, theta0, delta, earthG);
@@ -26,6 +28,7 @@ public class PendulumRunner {
 	// print out difference in displacement in 1 second intervals
 	// for 20 seconds
 	int iterations = (int) (1/delta);
+	System.out.println ("This is Earth");
 	System.out.println ("analytical vs. numerical displacement (fine, coarse)");
 	for (int second = 1; second <= 20; second++) {
 	    for (int i = 0; i < iterations; i++) rp.step ();
@@ -36,6 +39,30 @@ public class PendulumRunner {
 				nf.format (Math.toDegrees (rp.getLastTheta ()))
 				+ "\t" + 
 				nf.format (Math.toDegrees (rpCoarse.getLastTheta ())));
+	    
+	}
+	
+	RegularPendulum rpj = new RegularPendulum (sLen, pMass, theta0, delta, jupiterG);
+	SimplePendulum spj = new SimplePendulum (sLen, pMass, theta0, jupiterG);
+	RegularPendulum rpCoarsej = 
+	    new RegularPendulum (sLen, pMass, theta0, .1, jupiterG);
+
+	// print out difference in displacement in 1 second intervals
+	// for 20 seconds
+	System.out.println ("\nThis is Jupiter");
+	System.out.println ("analytical vs. numerical displacement (fine, coarse)");
+	for (int second = 1; second <= 20; second++) {
+	    for (int i = 0; i < iterations; i++) rpj.step ();
+	    for (int i = 0; i < 10; i++) rpCoarsej.step (); 
+	    System.out.println ("t=" + second + "s: \t" + 
+				nf.format (Math.toDegrees (spj.getTheta (second))) 
+				+ "\t" +
+				nf.format (Math.toDegrees (rpj.getLastTheta ()))
+				+ "\t" + 
+				nf.format (Math.toDegrees (rpCoarsej.getLastTheta ())));
+	
+	
+	
 	}
     }
 }
